50effb2f599d3becb32ba1cfb75efbc176c839aa
Kassen, Ted <Ted_Kassen@intuit.com>
2017-06-20 11:16:29 -0700

    Credit Cards are validating

--- hw1/src/CreditCardValidate.hs
+++ hw1/src/CreditCardValidate.hs
@@ -1,9 +1,17 @@
 module CreditCardValidate where
 
-toDigits :: Integer -> [Integer]
+toDigits :: Integral t => t -> [t]
 toDigits n
-  | n >= 10   = [1, (n - 10)]
-  | otherwise = [n]
+ | n < 1 = []
+ | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
 
-toDigitsRev :: Integer -> [Integer]
+toDigitsRev :: Integral t => t -> [t]
 toDigitsRev n = (foldl (flip(:)) []) (toDigits n)
+
+doubleEveryOther :: Integral t => [t] -> [t]
+doubleEveryOther = zipWith ($) (cycle [id, (*2)])
+
+sumDigits :: [Integer] -> Integer
+sumDigits n = sum (map sum (map toDigits n))
+
+validate = (((==) 0) . (\x -> mod x 10) . sumDigits . doubleEveryOther . toDigitsRev)
