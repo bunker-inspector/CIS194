3b3239ab491a9108f95cdbc95be5d6dff1695a5e
Ted Kassen <tedgkassen@gmail.com>
2017-06-23 22:27:47 -0700

    That was way harder than expected

--- hw2/src/LogAnalysis.hs
+++ hw2/src/LogAnalysis.hs
@@ -1,18 +1,17 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 
-import Data.List.Split
 import Log
 
-splitBy :: String -> Char -> [String]
-splitBy (x:xs) d
-
-
 parseMessage :: String -> LogMessage
-parseMessage n =
-                let x = splitOn " " n
-                in case x of
-                _  | ("I":timestamp:message)            -> LogMessage Info second message
-                   | ("E":severity:timestamp:message)   -> LogMessage (Error second) third rest
-                   | ("W":timestamp:message)            -> LogMessage Warning second message
-                   | otherwise                          -> Unknown n
+parseMessage [] = Unknown ""
+parseMessage [_] = Unknown ""
+parseMessage [_,_] = Unknown ""
+parseMessage n = let x = words n
+                 in case x of
+                  (code:second:third:rest)
+                    | code == "I" -> LogMessage Info (read second :: Int) (unwords (third : rest))
+                    | code == "E" -> LogMessage (Error (read second :: Int)) (read third :: Int) (unwords rest)
+                    | code == "W" -> LogMessage Warning (read second :: Int) (unwords (third : rest))
+                    | otherwise   -> Unknown n
+
