7c9ef2f54b613f8bec13f8415c919e024954059f
Kassen, Ted <Ted_Kassen@intuit.com>
2017-06-27 21:58:21 -0700

    Progress

--- hw2/src/LogAnalysis.hs
+++ hw2/src/LogAnalysis.hs
@@ -2,16 +2,27 @@
 module LogAnalysis where
 
 import Log
+import Data.List.Split
 
 parseMessage :: String -> LogMessage
-parseMessage [] = Unknown ""
-parseMessage [_] = Unknown ""
-parseMessage [_,_] = Unknown ""
 parseMessage n = let x = words n
                  in case x of
-                  (code:second:third:rest)
-                    | code == "I" -> LogMessage Info (read second :: Int) (unwords (third : rest))
-                    | code == "E" -> LogMessage (Error (read second :: Int)) (read third :: Int) (unwords rest)
-                    | code == "W" -> LogMessage Warning (read second :: Int) (unwords (third : rest))
-                    | otherwise   -> Unknown n
+                  ("I":timestamp:rest)          -> LogMessage Info (read timestamp) (unwords rest)
+                  ("E":severity:timestamp:rest) -> LogMessage (Error (read severity)) (read timestamp) (unwords rest)
+                  ("W":timestamp:rest)          -> LogMessage Warning (read timestamp) (unwords rest)
+                  _ -> Unknown n
 
+parse :: String -> [LogMessage]
+parse n = foldl (\xs x -> (parseMessage x) : xs) [] ys
+          where
+            ys = splitOn "\n" n
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert m t = case m of
+             (LogMessage _ s _) -> case t of
+                                   Leaf -> Node Leaf m Leaf
+                                   (Node l c@(LogMessage _ d _) r)
+                                     | s > d -> Node l c (insert m r)
+                                     | s < d -> Node (insert m l) c r
+                                     | _     -> t
+                              _ -> t
