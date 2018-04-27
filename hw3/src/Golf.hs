1d72806b3e6e8d6137efa227dc72955a0bf2963a
Ted Kassen <ted.kassen@streamlabs.com>
2018-04-26 19:37:03 -0700

    part of 3

--- hw3/src/Golf.hs
+++ hw3/src/Golf.hs
@@ -1,6 +1,8 @@
 module Golf where
 
 import Data.List.Split
+import qualified Data.HashMap as Map
+import Data.List
 
 skips :: [a] -> [[a]]
 skips xs = case xs of
@@ -16,3 +18,6 @@ localMaxima xs = case xs of
                                                              | second > first && second > (head rest) -> x ++ [second]
                                                            _ -> x) [] [0 .. ((length xs) - 1)]
                    _ -> xs
+
+freq1to9 :: [Integer] -> Map.Map Integer Integer
+freq1to9 xs = Map.fromList $ map (\x -> (x, (\y -> genericLength . filter (y==)) x xs)) [0..9]
